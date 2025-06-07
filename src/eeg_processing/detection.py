5e24f91e94ade48f2fb9c6720185f044b12722b1
Louis-Etienne Messier <lemes3@ulaval.ca>
2025-06-07 17:32:30 -0400

    fix: changing the flappy_control

--- src/eeg_processing/detection.py
+++ src/eeg_processing/detection.py
@@ -1,9 +1,24 @@
 import numpy as np
 from eeg_processing.filtering import butter_lowpass_filter
 
-BLINK_THRESHOLD = 75  # MIGHT NEED TO UPDATE THIS VALUE
+BLINK_THRESHOLD_OVER_CIRCULAR_BUFFER = 2 # MIGHT NEED TO UPDATE THIS VALUE
 
+CIRCULAR_BUFFER = [25] * 800
+CIRCULAR_BUFFER_SIZE = 800
 
+def add_element_to_circular_buffer(value):
+    if len(CIRCULAR_BUFFER) < CIRCULAR_BUFFER_SIZE:
+        CIRCULAR_BUFFER.append(value)
+    else:
+        CIRCULAR_BUFFER.pop()
+        CIRCULAR_BUFFER.insert(0, value)
+
+def get_circular_buffer_average():
+    valeur = np.mean(CIRCULAR_BUFFER)
+    if valeur < 25:
+         return 25
+    else:
+         return valeur
 
 def detect_blink(data):
     """Detects a blink in the EEG signal by analyzing amplitudes."""
@@ -12,4 +27,14 @@ def detect_blink(data):
     
     filtered_signal = data 
     filtered_signal = np.absolute(filtered_signal[:2])
-    return np.max(filtered_signal) > BLINK_THRESHOLD
+    max_signal = np.max(filtered_signal)
+    threshold = get_circular_buffer_average() * BLINK_THRESHOLD_OVER_CIRCULAR_BUFFER
+    print(f"buffer average : {get_circular_buffer_average()}")
+    
+    if max_signal > threshold:
+            print(f"jumped! : {max_signal}")
+    if max_signal < 100:
+        add_element_to_circular_buffer(max_signal)
+    
+
+    return max_signal > threshold
